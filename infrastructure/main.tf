95fac9f9ca1079594f6e42195ec1c9c86335127a
lvsun <lvsun@me.com>
2019-09-26 23:23:34 +0200

    allowed ssh traffic in vpc network

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,16 +4,22 @@ provider "google" {
   zone    = "europe-west4-c"
 }
 
+# vm instance
 resource "google_compute_instance" "vm_instance" {
   name         = "peisun-instance"
   machine_type = "n1-standard-1"
 
-  tags = ["project", "visum_in_germany"]
+  tags = ["project", "visum-in-germany"]
 
   boot_disk {
     initialize_params {
       image = "gce-uefi-images/ubuntu-1804-lts"
+      size  = "10"
     }
+  }
+
+  metadata = {
+    ssh-keys = "peisun:${file("~/.ssh/peisun_rsa.pub")}"
   }
 
   network_interface {
@@ -23,8 +29,20 @@ resource "google_compute_instance" "vm_instance" {
   }
 }
 
+# vpc network
 resource "google_compute_network" "vpc_network" {
   name                    = "peisun-network"
   auto_create_subnetworks = "true"
 }
 
+resource "google_compute_firewall" "allow_ssh" {
+  name          = "vm-allow-ssh"
+  network       = "${google_compute_network.vpc_network.name}"
+  direction     = "INGRESS"
+  source_ranges = ["84.143.148.143/32"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+}
